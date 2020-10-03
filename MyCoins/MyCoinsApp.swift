191d7d6dcdf5995f50bdb569a72bd01e0d72a54d
Roman Korostenskyi <romankorostensky@gmail.com>
2020-10-03 15:49:38 +0300

    chore: init commit

---
+++ MyCoins/MyCoinsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyCoinsApp.swift
+//  MyCoins
+//
+//  Created by Roman Korostenskyi on 03.10.2020.
+//
+
+import SwiftUI
+
+@main
+struct MyCoinsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
